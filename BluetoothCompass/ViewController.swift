f9c66662f1fc0917bf81c3a339072c75803b9074
Luke Bousfield <tech.master.champ@gmail.com>
2018-04-03 17:11:31 -0600

    Added bluetooth compass app

--- BluetoothCompass/ViewController.swift
+++ BluetoothCompass/ViewController.swift
@@ -6,20 +6,90 @@
 //  Copyright © 2018 Luke Bousfield. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+import Starscream;
+import CoreLocation;
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, WebSocketDelegate {
+    
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("Websocket connected.")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("Websocket disconnected.")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("Recieved message: \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("Recieved data: \(data)")
+    }
+    
+    
+    let serverLocation : String = "ws://192.168.0.108:8080"
+    
+    var isLocationAuthed = false
+    
+    var locationManager : CLLocationManager!
+    
+    var socket : WebSocket? = nil
+    
+    @IBOutlet weak var angleIndicator: UILabel!
+    
+    @IBAction func scan(_ sender: Any) {
+        print("Connecting to server.")
+        socket = WebSocket(url: URL(string: serverLocation)!)
+        socket!.delegate = self
+        socket!.connect()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager!.requestWhenInUseAuthorization()
+        locationManager!.startUpdatingHeading()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func send(num : Int) {
+        
+        if let s = socket {
+            s.write(string: "\(num)")
+        }
+        
+    }
+    
+    func locationManager(_ manager : CLLocationManager, didUpdateHeading newHeading : CLHeading) {
+        print("New heading: \(newHeading.trueHeading)")
+        let trueHeading : Int = Int(newHeading.trueHeading.rounded())
+        angleIndicator.text = "Angle: \(trueHeading)˚"
+        var toSend : Int = 0
+        var shouldSend : Bool = true
+        if (trueHeading < 270 && trueHeading > 90) {
+            print("Can't reach direction.")
+            shouldSend = false
+        } else if (trueHeading >= 270) {
+            toSend = -1 * (360 - trueHeading)
+        } else if (trueHeading <= 90) {
+            toSend = trueHeading
+        } else {
+            print("Something went very wrong.")
+        }
+        
+        if (shouldSend) {
+            send(num: toSend)
+        }
+        
+    }
 
 }
 
---
+++ Pods/Target Support Files/Pods-BluetoothCompass/Pods-BluetoothCompass-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BluetoothCompass : NSObject
+@end
+@implementation PodsDummy_Pods_BluetoothCompass
+@end
